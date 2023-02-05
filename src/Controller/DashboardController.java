209dc1d182f03f36f002d297a096d9dcefbea419
LabUser <cdrap27@wgu.edu>
2023-02-05 06:28:33 -0700

    a

---
+++ src/Controller/DashboardController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class DashboardController {
+    public void onWeaponsBase(ActionEvent actionEvent) {
+    }
+
+    public void onGearBase(ActionEvent actionEvent) {
+    }
+}
