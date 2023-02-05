77ee0a203d67f56e1a4a5b820043acb3297eb401
LabUser <cdrap27@wgu.edu>
2023-02-05 09:32:02 -0700

    a

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,10 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        BufferedReader br = Files.newBufferedReader(Path.of("modded.csv"), StandardCharsets.US_ASCII);
         Parent root = FXMLLoader.load(getClass().getResource("../Views/Dashboard.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 997, 587));
